a5f6695d86b65c7e963beac992bddde27e2db5ab
ricardomelogt <ricardofrontend@gmail.com>
2022-04-30 12:34:40 -0300

    Implementação da responsividade para dispositivos desktop médios < 1200px

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import { Sidebar } from './components/Sidebar/Sidebar';
 import { Header } from './components/Header/Header';
 import { Content } from './components/Content/Content';
-import { Footer } from './components/Footer/Footer'
+import { Footer } from './components/Footer/Footer';
+import './Responsive.css';
 
 function App() {
   return (
