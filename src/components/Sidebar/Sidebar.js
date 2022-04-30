5d61e1f86d4099aca2819545930ac2cf22d485f8
ricardomelogt <ricardofrontend@gmail.com>
2022-04-30 13:11:18 -0300

    Implementação do menu mobile

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,17 +2,22 @@ import './Sidebar.css';
 import Logo from '../../img/Logo.png';
 
 export const Sidebar = () => {
+
+    const toggleMenu = () => {
+        document.getElementById('sidebar_menu').classList.toggle('hidden');
+    }
+
     return(
         <aside className='sidebar_container'>
 
-            <div>
-                <h1>SLACA 2019</h1>
+            <div >
+                <h1>SLACA 2019 <i onClick={toggleMenu} className="bi bi-list"></i></h1>
             </div>
             
             <img alt='Logo SLACA 2019' src={Logo}/>
 
             <nav>
-                <ul>
+                <ul id="sidebar_menu">
                     <li>Apresentação</li>
                     <li>Comitês</li>
                     <li>Autores</li>
